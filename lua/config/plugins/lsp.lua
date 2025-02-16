18338579d2d48061f97403b274ef272c1de97359
rabin-giri <rabin.bex@gmail.com>
2025-02-16 21:02:42 +0545

    new feature added

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -34,6 +34,9 @@ return {
                         lspconfig.clangd.setup({
                                  capabilities=capabilities
                         })
+                        lspconfig.asm-lsp.setup({
+                                 capabilities=capabilities
+                        })
                         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
                         vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
                         vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
